9aef5f4bc9707fa0351b7aadc1849a558f37edd6
abelardolopezquintana <75378297+abelardolopezquintana@users.noreply.github.com>
2021-08-09 05:17:27 +0200

    Add files via upload

---
+++ zublime_payment_extra_fee/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © <2021> 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Payment",
+    "version": "1.0",
+    "author": "Abelardo",
+    "website": "",
+    "category": "account",
+    "complexity": "normal",
+    "description": """ """,
+    "depends": ["account","account_accountant"],
+    "data": [
+        "views/zublime_payment_extra_fee_view.xml",
+        # "views/menu_view.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
