e3266e13e099f5c50d473d1eff5e57b1d61f488b
Bernardo Justiz Gonzalez <bjustizglez@gmail.com>
2021-08-11 03:21:26 -0400

    [FIX] zublime_payment_extra_fee: Se visualiza el campo extra fee si tiene valor >0 en el payment.register. Se llama al super en vez de redefinir el método _create_payment_vals_from_wizard

--- zublime_payment_extra_fee/models/zublime_payment_extra_fee.py
+++ zublime_payment_extra_fee/models/zublime_payment_extra_fee.py
@@ -14,40 +14,17 @@ class AccountPaymentRegister(models.TransientModel):
     _inherit = 'account.payment.register'
 
     tariff = fields.Float(string="Tariff %", )
+    fee = fields.Float(
+        related='l10n_mx_edi_payment_method_id.tariff',
+    )
+     
+    @api.onchange('amount', 'l10n_mx_edi_payment_method_id')
+    def _onchange_amount(self):
+        self.tariff = self.amount * (self.l10n_mx_edi_payment_method_id.tariff / 100)
 
     def _create_payment_vals_from_wizard(self):
-        l10n_mx_edi_payment_method_id = self.env['l10n_mx_edi.payment.method'].search(
-            [('id', '=', self.l10n_mx_edi_payment_method_id.id)], limit=1)
-        if l10n_mx_edi_payment_method_id.tariff > 0:
-            tariff = self.amount * (l10n_mx_edi_payment_method_id.tariff / 100)
-        else:
-            tariff = 0
-        self.tariff = tariff
-        payment_vals = {
-            'date': self.payment_date,
-            'amount': self.amount,
-            'payment_type': self.payment_type,
-            'partner_type': self.partner_type,
-            'ref': self.communication,
-            'journal_id': self.journal_id.id,
-            'currency_id': self.currency_id.id,
-            'partner_id': self.partner_id.id,
-            'partner_bank_id': self.partner_bank_id.id,
-            'payment_method_id': self.payment_method_id.id,
-            'tariff': tariff,
-            'destination_account_id': self.line_ids[0].account_id.id
-        }
-
-        if not self.currency_id.is_zero(self.payment_difference) and self.payment_difference_handling == 'reconcile':
-            payment_vals['write_off_line_vals'] = {
-                'name': self.writeoff_label,
-                'amount': self.payment_difference,
-                'account_id': self.writeoff_account_id.id,
-            }
-        data = {
-            'options': [payment_vals, tariff],
-        }
-        # return self.env.ref('zublime_payment_extra_fee.zublime_payment_extra_fee_id').report_action([], data=data)
+        payment_vals = super(AccountPaymentRegister, self)._create_payment_vals_from_wizard()
+        payment_vals['tariff'] = self.tariff
         return payment_vals
 
 
@@ -55,3 +32,10 @@ class AccountPayment(models.Model):
     _inherit = "account.payment"
 
     tariff = fields.Float(string="Tariff")
+    fee = fields.Float(
+        related='l10n_mx_edi_payment_method_id.tariff',
+    )
+
+    @api.onchange('amount', 'l10n_mx_edi_payment_method_id')
+    def _onchange_amount(self):
+        self.tariff = self.amount * (self.l10n_mx_edi_payment_method_id.tariff / 100)
