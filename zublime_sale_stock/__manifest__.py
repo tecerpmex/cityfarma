cddedee59533fb2513e52f1c2f922a127e7d328b
Bernardo Justiz Gonzalez <bjustizglez@gmail.com>
2021-09-30 19:35:55 -0400

    [ADD] zublime_sale_stock: Se agrega advertencia al admin al confirmar pedidos sin disponibilidad.

--- zublime_sale_stock/__manifest__.py
+++ zublime_sale_stock/__manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-
+        'security/ir.model.access.csv',
+        'views/sale_order_views.xml',
     ],
     # only loaded in demonstration mode
     'application': True,
