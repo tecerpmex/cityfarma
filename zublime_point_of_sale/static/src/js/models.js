5aafaae35d6eab9645a8d1d6fcd3f7c5ba095a2d
Abelardo <abelardo.lopez@desoft.cu>
2021-10-05 23:22:10 +0200

    Requerimiento 3 - PV

--- zublime_point_of_sale/static/src/js/models.js
+++ zublime_point_of_sale/static/src/js/models.js
@@ -18,7 +18,10 @@ odoo.define('zublime_pos_extra_fee.models', function (require) {
         },
         get_amount_with_extra_fee: function () {
             return round_pr(this.amount + this.get_extra_fee(), this.pos.currency.rounding);
-        },      
+        },
+        get_extra_fee_dc: function () {
+            return this.payment_method.extra_fee / 1;
+        },
     });
 
     const _super_order = models.Order.prototype;
@@ -27,12 +30,18 @@ odoo.define('zublime_pos_extra_fee.models', function (require) {
             const self = this;
             const receipt = _super_order.export_for_printing.apply(this, arguments);
             receipt.total_extra_fee = self.get_total_extra_fee();
+            receipt.extra_fee1 =self.get_extra_fee_pc();
             return receipt;
         },
         get_total_extra_fee: function() {
             return round_pr(this.paymentlines.reduce((function(sum, paymentLine) {
                 sum += paymentLine.get_extra_fee();
                 return sum;
+            }), 0), this.pos.currency.rounding);
+        },
+         get_extra_fee_pc: function() {
+         return round_pr(this.paymentlines.reduce((function(s, paymentLine) {
+                return paymentLine.get_extra_fee_dc();
             }), 0), this.pos.currency.rounding);
         },
     });
