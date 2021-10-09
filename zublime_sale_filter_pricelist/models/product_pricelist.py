fecf52ca179fe3078c9d795b1ddb09eef9692418
Bernardo Justiz Gonzalez <bjustizglez@gmail.com>
2021-10-09 00:01:38 -0400

    [FIX] zublime_sale_filter_pricelist: Se mejora accion mostrar lista de precios y elementos de listas de precios

--- zublime_sale_filter_pricelist/models/product_pricelist.py
+++ zublime_sale_filter_pricelist/models/product_pricelist.py
@@ -7,14 +7,14 @@ class Pricelist(models.Model):
     _inherit = "product.pricelist"
 
     def get_pricelist_item(self):
-        action = self.env['ir.actions.act_window']._for_xml_id('product.product_pricelist_item_action')
-        ctx = dict(self.env.context)
-        ctx.pop('active_id', None)
-        ctx['active_model'] = 'product.pricelist.item'
-        ctx['domain'] = [('pricelist_id', '=', self.id)]
-        action['context'] = ctx,
-        action.update(
-            context=dict(default_pricelist_id=self.id),
-            domain=[('pricelist_id', '=', self.id)]
-        )
-        return  action
+        view_id = self.env.ref('product.product_pricelist_item_tree_view_from_product').id
+        return {
+            'name': 'List Prices Items',
+            'view_mode': 'tree',
+            'views': [[view_id, 'tree']],
+            'res_model': 'product.pricelist.item',
+            'type': 'ir.actions.act_window',
+            'context': {'default_pricelist_id': self.id},
+            'domain': [('pricelist_id', '=', self.id)],
+            'target': 'current',
+        }
