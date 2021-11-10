4b090b9254d8a0262c1bdd51c3b487ba18c4b9ff
Sistemas CityFarma/CityMark <sistemas@cityfarma.com.mx>
2021-11-09 20:39:47 -0600

    Update product_pricelist_item.py

--- zublime_sale_pricelist/models/product_pricelist_item.py
+++ zublime_sale_pricelist/models/product_pricelist_item.py
@@ -32,7 +32,8 @@ class ProductPriceListItem(models.Model):
         related='product_tmpl_id.taxes_id'
         )
     barcode = fields.Char(
-        #related='product_id.barcode',
+        #related='product_id.barcode', # fix barcode doesnt show
+        #store=True, # fix barcode doesnt show
         related='product_tmpl_id.barcode',
         readonly=True
         )
